e261f580271c33af4017a0320b72208a4fa5b6df
Crowdbotics <team@crowdbotics.com>
2024-02-13 16:22:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solar-education-46303",
+  url: "https://solar-education-46303.botics.co",
+  api: "https://solar-education-46303.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
